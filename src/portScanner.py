a38c4b36bfdccb6b44f5eabd7cdd67dadc48cc6c
cdcorro <53372649+cdcorro@users.noreply.github.com>
2020-12-19 14:36:30 -0500

    faster portScanner

--- src/portScanner.py
+++ src/portScanner.py
@@ -1,45 +1,56 @@
-import sys 
-import socket 
-from datetime import datetime 
+import socket, threading
+from datetime import datetime
 
-def portScanner(IP):
+def PortScanner(host_ip = '172.217.1.164'):
+
+        connections = []        # To run connections at the same time
+        result = {}         # all
+        OpenPorts = []
+
         # translate hostname to IPv4 
-        ip = socket.gethostbyname(IP) 
+        ip = socket.gethostbyname(host_ip) 
 
         #prints status block of target and when the scan starts
         print("-" * 50) 
         print("Scanning: " + ip) 
-        print("Scanning started at:" + str(datetime.now()).split('.')[0]) 
+        print("Scanning began at: " + str(datetime.now()).split('.')[0])
+        print("**approximate runtime is 1 minute 30 seconds**") 
         print("-" * 50) 
+        
+        # Spawning threads to scan ports
+        for a in range(65535):
+                t = threading.Thread(target=TCP_connect, args=(ip, a, result))
+                connections.append(t)
 
-        try: 
-                # scan ports between 1 to 65,535
-                openPorts = []
-                for port in range(1,65535):
+        # Starting threads
+        for b in range(65535):
+                connections[b].start()
 
-                        # create socket
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        socket.setdefaulttimeout(1) 
-                        
-                        # s.connect_ex() returns 0 if port is open 
-                        c = s.connect_ex((ip,port))
-                        if c == 0: 
-                                print("Port",port,"is open")
-                                openPorts.append(port)
-                        s.close()
+        # wait until all threads complete
+        for c in range(65535):
+                connections[c].join()
 
-                print(" List of Open Ports:",openPorts)
-                        
-        except KeyboardInterrupt: 
-                        print("\n Scan Stopped.") 
-                        sys.exit() 
-        except socket.gaierror: 
-                        print("\n Hostname could not Be Resolved.") 
-                        sys.exit() 
-        except socket.error: 
-                        print("\ Server could not be reached.") 
-                        sys.exit()
+        # Printing open ports
+        for d in range(65535):
+                if result[d] == 'open':
+                    print("Port",d,'is',result[d])
+                    OpenPorts.append(d)
+        
+        #print open ports in a list
+        print("\nThe Open Ports are:",OpenPorts)
+        
+        #prints finish time
+        print("\nScanning has finished at ",str(datetime.now()).split('.')[0])
 
-#test code
-IPadd = input("Please enter IP address: ")
-portScanner(IPadd)
+def TCP_connect(ip, port, result):
+        TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        TCPsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        TCPsock.settimeout(1)
+        try:
+                TCPsock.connect((ip, port))
+                result[port] = 'open'
+        except:
+                result[port] = ''
+
+host_ip = input("Enter host IP: ")
+PortScanner(host_ip)
